fd4f04712393d0a50404cc4af07be11ac99177c2
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-03-22 19:20:52 -0300

    Requisito 03 Ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
+import App from '../App';
 import { FavoritePokemons } from '../components';
 
 describe('3. Teste o componente <FavoritePokemons.js />', () => {
@@ -12,5 +13,16 @@ describe('3. Teste o componente <FavoritePokemons.js />', () => {
     if (!img) {
       expect(screen.getByText(/no favorite pokemon found/i));
     }
+  });
+
+  it('Verifica se os marcados como favoritos são exibidos na página', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+    const checked = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checked);
+    const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(linkFavorite);
+    expect(screen.getByText(/pikachu/i));
   });
 });
