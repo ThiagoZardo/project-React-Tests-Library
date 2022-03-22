3e1cb70c0e15d3594ab9dafb9e56f1ae1904286b
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-03-22 19:48:17 -0300

    Requisito 04 ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  it('Verifica se página contém um heading h2 com o texto '
+  + 'Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    const titleNotFound = screen.getByRole('heading',
+      { name: /Page requested not found Crying emoji/i },
+      { level: 2 });
+    expect(titleNotFound).toBeInTheDocument();
+  });
+
+  it('Verifica se exibe a imagem correta', () => {
+    renderWithRouter(<NotFound />);
+    const imgNotFound = screen.getByRole('img',
+      { name: /pikachu crying because the page requested was not found/i });
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
