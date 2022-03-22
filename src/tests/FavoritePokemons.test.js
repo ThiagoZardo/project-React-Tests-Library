46c35f93346d08521f50219503ddad68f18d9ac2
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-03-22 19:03:21 -0300

    Iniciando Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('3. Teste o componente <FavoritePokemons.js />', () => {
+  it('Verifica se é exibida mensagem No favorite '
+  + 'pokemon found, se a pessoa não tiver pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const img = screen.queryByRole('img');
+    if (!img) {
+      expect(screen.getByText(/no favorite pokemon found/i));
+    }
+  });
+});
